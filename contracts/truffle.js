9a08e1a8e66969b0a62bd7661a07cf60c9bb39e5
ninantharakan <ninan.tharakan@gmail.com>
2018-02-23 19:50:02 +0000

    Create truffle.js

---
+++ contracts/truffle.js
@@ -0,0 +1,1 @@
+module.exports = {  networks: {    development: {      host: "metastock-ninantharakan.c9users.io",      port: 8080,    // gas: 500000 ,      network_id: "*" // Match any network id          }  }};
